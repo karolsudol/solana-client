ed77150ab6f0326c98a83bdedf20b36a10203781
elcomendante <karol@finsane.com>
2023-02-20 11:12:24 +0000

    built and deployed devnet

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # solana-client
+
 basic solana native program and client
+
+## config - build - deploy
+
+`solana-keygen new --no-bip39-passphrase`
+
+`solana config set --keypair /root/.config/solana/id.json`
+
+`solana config set --url http://api.devnet.solana.com`
+
+`cargo init program --lib`
+
+`cargo add solana-program`
+
+`cargo-build-sbf --arch bpf `
+
+`solana program deploy target/deploy/solana_client.so`
+
+## Program Id: HGPydrVpNajDC2BjepAeMyM6PQPoVUnxj1jbN54SfLLp
--- src/program/src/lib.rs
+++ src/program/src/lib.rs
@@ -2,13 +2,18 @@ use solana_program::{
     account_info::AccountInfo, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey,
 };
 
+// declare and export the program's entrypoint
 entrypoint!(process_instruction);
 
-fn process_instruction(
+// program entrypoint's implementation
+pub fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!("Hello Solana! (From Rust!)");
+    // log a message to the blockchain
+    msg!("Hello, world!");
+
+    // gracefully exit the program
     Ok(())
 }
